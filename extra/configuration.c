5505fcd424534a81a52f7568a65749b7ce689974
mihai <myhay.360@gmail.com>
2015-06-10 17:24:34 +0200

    Insertar librería de configuración (libconfig) e instalarla con el ./install. Crear el archivo de configuración por defecto y conseguir leer el fichero. Configucarión Separada.

--- extra/configuration.c
+++ extra/configuration.c
@@ -23,72 +23,96 @@
 #include <string.h>
 
 
-int main()
-{
-    config_t cfg;
-    config_setting_t *setting;
-    int speep_time;
+typedef struct ClientCardTag{
 
-    config_init(&cfg);
+    const char *pcm_name;
+    int frame_Rate, pcm_buffer_size, pcm_period_size, buffer;
+
+} ClientCard;
+
+typedef struct ClientSoundTag{
+
+    const char *sound_folder;
+    int word_length, sounds_number;
+    char *sounds_list;
+
+} ClientSound;
+
+typedef struct ClientSpeekersTag{
+
+    int speekers_number, chanels_number;
+
+} ClientSpeekers;
+
+
+int checkConfig(config_t* cfg, char* configFile){
+
+    config_init(cfg);
 
     /* Read the file. If there is an error, report it and exit. */
-    if(! config_read_file(&cfg, "default.cfg"))
+    if(! config_read_file(cfg, configFile))
     {
-        fprintf(stderr, "%s:%d - %s\n", config_error_file(&cfg),
-                config_error_line(&cfg), config_error_text(&cfg));
-        config_destroy(&cfg);
+        fprintf(stderr, "%s:%d - %s\n", config_error_file(cfg),
+                config_error_line(cfg), config_error_text(cfg));
+        config_destroy(cfg);
         return(EXIT_FAILURE);
     }
+    return 0;
+}
 
-    /* Get the store name. */
-    if(config_lookup_int(&cfg, "sleep_time", &speep_time))
-        printf("Sleep Time: %d\n\n", speep_time);
-    else
-        fprintf(stderr, "No 'sleep_time' setting in configuration file.\n");
+struct ClientCardTag clientCardGenerator(config_t* cfg){
 
-    /* Output a list of all books in the inventory. */
-    setting = config_lookup(&cfg, "client");
+    struct ClientCardTag clientCardConfig;
+
+    /* Output card config. */
+    config_setting_t* setting = config_lookup(cfg, "client");
     if(setting != NULL) {
-
-
-        config_setting_t *card = config_lookup(&cfg, "client.card");
+        config_setting_t *card = config_lookup(cfg, "client.card");
         if (card != NULL) {
 
             /* Only output the record if all of the expected fields are present. */
-            const char *pcm_name;
-            int frame_Rate, pcm_buffer_size, pcm_period_size, buffer;
-
-            if (!(config_setting_lookup_string(card, "pcm_name", &pcm_name)
-                  && config_setting_lookup_int(card, "frame_Rate", &frame_Rate)
-                  && config_setting_lookup_int(card, "pcm_buffer_size", &pcm_buffer_size)
-                  && config_setting_lookup_int(card, "pcm_period_size", &pcm_period_size)))
+            if (!(config_setting_lookup_string(card, "pcm_name", &clientCardConfig.pcm_name)
+                  && config_setting_lookup_int(card, "frame_Rate", &clientCardConfig.frame_Rate)
+                  && config_setting_lookup_int(card, "pcm_buffer_size", &clientCardConfig.pcm_buffer_size)
+                  && config_setting_lookup_int(card, "pcm_period_size", &clientCardConfig.pcm_period_size)))
                 printf("Missing config Client Card!\n");
 
-            buffer = (pcm_buffer_size / 4);
-            printf("%-30s  %d  %d  %d  %d\n", pcm_name, frame_Rate, pcm_buffer_size, pcm_period_size, buffer);
+            clientCardConfig.buffer = (clientCardConfig.pcm_buffer_size / 4);
+            printf("%-30s  %d  %d  %d  %d\n", clientCardConfig.pcm_name, clientCardConfig.frame_Rate, clientCardConfig.pcm_buffer_size, clientCardConfig.pcm_period_size, clientCardConfig.buffer);
             putchar('\n');
         }
+    }
+    return clientCardConfig;
+}
 
 
-        config_setting_t *sound = config_lookup(&cfg, "client.sound");
+struct ClientSoundTag clientSoundGenerator(config_t* cfg){
+
+    struct ClientSoundTag soundConfig;
+
+    config_setting_t* setting = config_lookup(cfg, "client");
+    if(setting != NULL) {
+
+        config_setting_t *sound = config_lookup(cfg, "client.sound");
         if (sound != NULL) {
 
             /* Only output the record if all of the expected fields are present. */
-            const char *sound_folder;
-            int word_length, sounds_number;
+            //const char *sound_folder;
+            //int word_length, sounds_number;
 
-            if (!(config_setting_lookup_string(sound, "sound_folder", &sound_folder)
-                  && config_setting_lookup_int(sound, "word_length", &word_length)
-                  && config_setting_lookup_int(sound, "sounds_number", &sounds_number)))
+            if (!(config_setting_lookup_string(sound, "sound_folder", &soundConfig.sound_folder)
+                  && config_setting_lookup_int(sound, "word_length", &soundConfig.word_length)
+                  && config_setting_lookup_int(sound, "sounds_number", &soundConfig.sounds_number)))
                 printf("Missing config Client Sound!\n");
 
 
-            config_setting_t *soundlist = config_lookup(&cfg, "client.sound.sounds_list");
+            config_setting_t *soundlist = config_lookup(cfg, "client.sound.sounds_list");
             unsigned int count = config_setting_length(soundlist);
             unsigned int i;
 
             /*crear lista de canciones*/
-            char *sounds_list = (char *) calloc(sounds_number * word_length, sizeof(char));;
+
+            soundConfig.sounds_list = (char *) calloc(soundConfig.sounds_number * soundConfig.word_length, sizeof(char));;
 
 
             for (i = 0; i < count; ++i) {
@@ -100,40 +124,58 @@ int main()
                 if (!(config_setting_lookup_string(songName, "file_name", &file_name)))
                     continue;
 
-                char fileRoute[word_length];
-                strcpy(fileRoute, sound_folder);
+                char fileRoute[soundConfig.word_length];
+                strcpy(fileRoute, soundConfig.sound_folder);
                 strcat(fileRoute, file_name);
 
-                strcpy(sounds_list + i * word_length, fileRoute);
+                strcpy(soundConfig.sounds_list + i * soundConfig.word_length, fileRoute);
             }
             putchar('\n');
 
             int j;
-            for (j = 0; j < sounds_number; ++j) {
-                printf("%s\n", sounds_list+j*word_length);
+            for (j = 0; j < soundConfig.sounds_number; ++j) {
+                printf("%s\n", soundConfig.sounds_list + j * soundConfig.word_length);
             }
 
             putchar('\n');
         }
+    }
 
-        config_setting_t *speekers = config_lookup(&cfg, "client.speekers");
+    return soundConfig;
+
+}
+
+struct ClientSpeekersTag clientSpeekersGenerator (config_t* cfg){
+
+    struct ClientSpeekersTag speekersConfig;
+
+    /* Output speekers config. */
+    config_setting_t *setting = config_lookup(cfg, "client");
+    if(setting != NULL) {
+
+        config_setting_t *speekers = config_lookup(cfg, "client.speekers");
         if (speekers != NULL) {
             /* Only output the record if all of the expected fields are present. */
-            int speekers_number, chanels_number;
+            //int speekers_number, chanels_number;
 
-            if (!(config_setting_lookup_int(speekers, "speekers_number", &speekers_number)
-                  && config_setting_lookup_int(speekers, "chanels_number", &chanels_number)))
+            if (!(config_setting_lookup_int(speekers, "speekers_number", &speekersConfig.speekers_number)
+                  && config_setting_lookup_int(speekers, "chanels_number", &speekersConfig.chanels_number)))
                 printf("Missing config Speekers!\n");
 
-            printf("%d  %d\n", speekers_number, chanels_number);
+            printf("%d  %d\n", speekersConfig.speekers_number, speekersConfig.chanels_number);
             putchar('\n');
         }
     }
+    return speekersConfig;
+}
 
-    setting = config_lookup(&cfg, "server");
+int seerverInicialConfig(config_t* cfg){
+
+    config_setting_t *setting = config_lookup(cfg, "server");
+    int clients_number;
+
     if(setting != NULL)
     {
-        int clients_number;
 
         if(!(config_setting_lookup_int(setting, "clients_number", &clients_number) ) )
             printf("Faltan clients_number!\n");
@@ -142,6 +184,47 @@ int main()
 
         putchar('\n');
     }
+    return clients_number;
+}
+
+
+int main()
+{
+    config_t cfg;
+
+    printf("inicio config\n");
+
+    char* configFile= "default.cfg";
+
+    checkConfig(&cfg,configFile);
+
+    int time_to_start;
+    /* Get the time to start. */
+    if(config_lookup_int(&cfg, "time_to_start", &time_to_start))
+        printf("Time to start: %d\n\n", time_to_start);
+    else
+        fprintf(stderr, "No 'time_to_start' setting in configuration file.\n");
+
+    // Read config Card
+    struct ClientCardTag card = clientCardGenerator(&cfg);
+    // Print config Card
+    printf("%-30s  %d  %d  %d  %d\n", card.pcm_name, card.frame_Rate, card.pcm_buffer_size, card.pcm_period_size, card.buffer);
+
+    // Read config Sound
+    struct ClientSoundTag sound = clientSoundGenerator(&cfg);
+
+    // Print config Sound
+    printf("%-30s  %d  %d \n", sound.sound_folder, sound.sounds_number, sound.word_length);
+    int j;
+    for (j = 0; j < sound.sounds_number; ++j) {
+        printf("%s\n", sound.sounds_list+j*sound.word_length);
+    }
+
+
+    struct ClientSpeekersTag speekers = clientSpeekersGenerator(&cfg);
+    printf("%d  %d\n", speekers.speekers_number, speekers.chanels_number);
+
+    printf("%d\n", seerverInicialConfig(&cfg));
 
     config_destroy(&cfg);
     return(EXIT_SUCCESS);
